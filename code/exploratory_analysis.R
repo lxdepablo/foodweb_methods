dcfde3e9eaa31439fe819c783ee1028935ff8254
lxdepablo <luisxdepablo@gmail.com>
2024-04-01 10:16:16 -0600

    added code to read in raw data

--- code/exploratory_analysis.R
+++ code/exploratory_analysis.R
@@ -3,5 +3,8 @@ library(tidyverse)
 
 # set working directory
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+setwd("../")
 
 # read in raw data
+data_raw <- read_csv("data/gateway-cleaned.csv")
+
